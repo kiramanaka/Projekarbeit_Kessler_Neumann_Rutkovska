d48e922e0afe7b79e1704d682df9262280f29eff
Kira Neumann <46658007+kiramanaka@users.noreply.github.com>
2024-12-10 13:51:05 +0100

    --KIRA-- - implement user management with proper user roles in app.py - create RegisterForm, ChangePasswordForm, ChangePermissionsForm in forms.py - add stub classes to app.py to enable editing and adding groups and children to db

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, redirect, request, url_for, flash, jsonify
-from flask_wtf import CSRFProtect
+from flask import Flask, flash, redirect, render_template, url_for
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from models import db, User, Child, Group
-from forms import LoginForm
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from flask_wtf import CSRFProtect
+from werkzeug.security import check_password_hash, generate_password_hash
 
+from forms import ChangePasswordForm, ChangePermissionsForm, LoginForm, RegisterForm
+from models import User, db
 
 """
 Initialization of the Flask app, the database, the CSRF protection, the login manager and the user loader.
@@ -34,8 +33,12 @@ def login():
         user = User.query.filter_by(username=form.username.data.lower()).first()
         if user:
             if check_password_hash(user.password_hash, form.password.data):
-                login_user(user)
-                return redirect(url_for('index'))
+                if user.permission_level == 2:
+                    flash('Dieser Benutzer ist deaktiviert', 'danger')
+                    return redirect(url_for('login'))
+                else:
+                    login_user(user)
+                    return redirect(url_for('index'))
             else:
                 flash('Ungültige Anmeldedaten')
         else:
@@ -58,10 +61,34 @@ def index():
     return render_template('index.html')
 
 
-@app.route(rule='/groups'):
+@app.route(rule='/groups')
 @login_required
 def groups():
     return render_template('groups.html')
+
+
+@app.route(rule='/groups/view/<int:group_id>')
+@login_required
+def view_group(group_id):
+    return render_template('view_group.html')
+
+
+@app.route(rule='/groups/edit/<int:group_id>', methods=['GET', 'POST'])
+@login_required
+def edit_group(group_id):
+    return render_template('edit_group.html')
+
+
+@app.route(rule='/edit_child/<int:child_id>', methods=['GET', 'POST'])
+@login_required
+def edit_child(child_id):
+    return render_template('edit_child.html')
+
+
+@app.route(rule='/new_child', methods=['GET', 'POST'])
+@login_required
+def new_child():
+    return render_template('new_child.html')
 
 
 @app.route(rule='/observations')
@@ -80,3 +107,67 @@ def view_observations(child_id):
 @login_required
 def edit_observations(child_id):
     return render_template('edit_observations.html')
+
+
+@app.route(rule='/user_management', methods=['GET', 'POST'])
+@login_required
+def user_management():
+    """
+    'user_management' allows administrators to create new users, change their passwords and change user roles.
+    """
+    if current_user.permmission_id != 1:
+        flash('Sie haben keine Berechtigung für diese Seite', 'danger')
+        return redirect(url_for('index'))
+    else:
+        form = RegisterForm()
+        if form.validate_on_submit():
+            # noinspection PyArgumentList
+            user = User(
+                username=form.username.data.lower(),
+                password_hash=generate_password_hash(form.password.data),
+                given_name=form.given_name.data,
+                surname=form.surname.data,
+                permission_level=form.permission_level.data
+            )
+            db.Session.add(user)
+            db.Session.commit()
+            flash('Benutzer erfolgreich angelegt', 'success')
+            return redirect(url_for('user_management'))
+        return render_template('user_management.html', form=form)
+
+
+@app.route(rule='/change_password/<int:userid>', methods=['GET', 'POST'])
+@login_required
+def change_password(userid):
+    if current_user.permission_id != 1 or current_user.userid != userid:
+        flash('Sie haben keine Berechtigung für diese Seite', 'danger')
+        return redirect(url_for('index'))
+    else:
+        form = ChangePasswordForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(userid=userid).first_or_404()
+            user.password_hash = generate_password_hash(form.password.data)
+            db.session.commit()
+            flash('Passwort erfolgreich geändert', 'success')
+            if current_user.permission_level == 1:
+                return redirect(url_for('user_management'))
+            else:
+                return redirect(url_for('index'))
+        return render_template('change_password.html', form=form)
+
+
+@app.route(rule='/change_permissions/<int:userid>', methods=['GET', 'POST'])
+@login_required
+def change_permissions(userid):
+    if current_user.permission_id != 1:
+        flash('Sie haben keine Berechtigung für diese Seite', 'danger')
+        return redirect(url_for('index'))
+    else:
+        form = ChangePermissionsForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(userid=userid).first_or_404()
+            user.permission_level = form.permission_level.data
+            db.session.commit()
+            flash('Berechtigungen erfolgreich geändert', 'success')
+            return redirect(url_for('user_management'))
+        return render_template('change_permissions.html', form=form)
--- forms.py
+++ forms.py
@@ -9,5 +9,31 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Anmelden')
 
 
+class RegisterForm(FlaskForm):
+    username = StringField('Benutzername', validators=[DataRequired()])
+    password = PasswordField('Passwort', validators=[DataRequired(), EqualTo('confirm_password',
+                                                                             message='Passwörter stimmen nicht überein')])
+    confirm_password = PasswordField('Passwort bestätigen', validators=[DataRequired()])
+    given_name = StringField('Vorname')
+    surname = StringField('Nachname')
+    permission_level = SelectField('Berechtigungsstufe', choices=[(0, 'Benutzer'), (1, 'Administrator')], coerce=int)
+    submit = SubmitField('Registrieren')
+
+
+class ChangePasswordForm(FlaskForm):
+    password = PasswordField('Neues Passwort', validators=[DataRequired()])
+    password2 = PasswordField('Passwort wiederholen', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Speichern')
+
+
+class ChangePermissionsForm(FlaskForm):
+    permission_level = SelectField('Berechtigungen', choices=[
+        (0, 'Benutzer'),
+        (1, 'Administrator'),
+        (2, 'Deaktiviert')],
+                                   coerce=int)
+    submit = SubmitField('Speichern')
+
+
 class ObservationForm(FlaskForm):
     pass
