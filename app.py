6c8040448131d953b8e38ab41a84b934979edce2
Kira Neumann <46658007+kiramanaka@users.noreply.github.com>
2024-12-10 13:51:05 +0100

    --KIRA-- - implement user management with proper user roles in app.py - create RegisterForm, ChangePasswordForm, ChangePermissionsForm in forms.py - add stub classes to app.py to enable editing and adding groups and children to db - change site structure in app.py to ease development and readability - create forms for editing and adding groups and children in forms.py - rename ChangePermissionsForm to EditPermissionsForm in forms.py to aid readability - rename ChangePasswordForm to EditPasswordForm in forms.py to aid readability - update requirements.txt to reflect inclusion of WTForms package

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, redirect, request, url_for, flash, jsonify
-from flask_wtf import CSRFProtect
+from flask import Flask, flash, redirect, render_template, url_for
 from flask_bootstrap import Bootstrap
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required, logout_user, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from models import db, User, Child, Group
-from forms import LoginForm
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user
+from flask_wtf import CSRFProtect
+from werkzeug.security import check_password_hash, generate_password_hash
 
+from forms import EditPasswordForm, EditPermissionsForm, EditGroupForm, LoginForm, RegisterForm
+from models import Group, User, db
 
 """
 Initialization of the Flask app, the database, the CSRF protection, the login manager and the user loader.
@@ -34,8 +33,12 @@ def login():
         user = User.query.filter_by(username=form.username.data.lower()).first()
         if user:
             if check_password_hash(user.password_hash, form.password.data):
-                login_user(user)
-                return redirect(url_for('index'))
+                if user.permission_level == 2:
+                    flash('Dieser Benutzer ist deaktiviert', 'danger')
+                    return redirect(url_for('login'))
+                else:
+                    login_user(user)
+                    return redirect(url_for('index'))
             else:
                 flash('Ungültige Anmeldedaten')
         else:
@@ -58,25 +61,140 @@ def index():
     return render_template('index.html')
 
 
-@app.route(rule='/groups'):
+@app.route(rule='/groups')
 @login_required
 def groups():
-    return render_template('groups.html')
+    group = Group.query.order_by(Group.group_name).all()
+    return render_template('groups.html', groups=group)
 
 
-@app.route(rule='/observations')
+@app.route(rule='/groups/view/<int:group_id>')
 @login_required
-def observations():
-    return render_template('observations.html')
+def view_group(group_id):
+    group = Group.query.filter_by(group_id=group_id).first_or_404()
+    return render_template('view_group.html')
 
 
-@app.route(rule='/observations/view/<int:child_id>')
+@app.route(rule='/groups/edit/<int:group_id>', methods=['GET', 'POST'])
 @login_required
-def view_observations(child_id):
+def edit_group(group_id):
+    form = EditGroupForm()
+    if form.validate_on_submit():
+        group = Group.query.filter_by(group_id=group_id).first_or_404()
+        group.group_name = form.group_name.data
+        group.children = form.children.data
+        db.session.commit()
+        flash('Gruppe erfolgreich geändert', 'success')
+        return redirect(url_for('groups'))
+    return render_template('edit_group.html')
+
+
+@app.route(rule='/groups/new', methods=['GET', 'POST'])
+@login_required
+def new_group():
+    return render_template('new_group.html')
+
+
+@app.route(rule='/children/view/<int:child_id>', methods=['GET', 'POST'])
+@login_required
+def view_child(child_id):
+    return render_template('view_child.html')
+
+
+@app.route(rule='/children/edit/<int:child_id>', methods=['GET', 'POST'])
+@login_required
+def edit_child(child_id):
+    return render_template('edit_child.html')
+
+
+@app.route(rule='/children/new', methods=['GET', 'POST'])
+@login_required
+def new_child():
+    return render_template('new_child.html')
+
+
+@app.route(rule='/observations/view/<int:observation_id>')
+@login_required
+def view_observations(observation_id):
     return render_template('view_observations.html')
 
 
-@app.route(rule="/observations/edit/<int:child_id>", methods=['GET', 'POST'])
+@app.route(rule="/observations/edit/<int:observation_id>", methods=['GET', 'POST'])
 @login_required
-def edit_observations(child_id):
+def edit_observations(observation_id):
     return render_template('edit_observations.html')
+
+
+@app.route(rule='/observations/new', methods=['GET', 'POST'])
+@login_required
+def new_observations():
+    return render_template('new_observations.html')
+
+
+@app.route(rule='/user_management', methods=['GET', 'POST'])
+@login_required
+def user_management():
+    """
+    'user_management' allows administrators to create new users, change their passwords and change user roles.
+    """
+    if current_user.permmission_id != 1:
+        flash('Sie haben keine Berechtigung für diese Seite', 'danger')
+        return redirect(url_for('index'))
+    else:
+        form = RegisterForm()
+        if form.validate_on_submit():
+            # noinspection PyArgumentList
+            user = User(
+                username=form.username.data.lower(),
+                password_hash=generate_password_hash(form.password.data),
+                given_name=form.given_name.data,
+                surname=form.surname.data,
+                permission_level=form.permission_level.data
+            )
+            db.Session.add(user)
+            db.Session.commit()
+            flash('Benutzer erfolgreich angelegt', 'success')
+            return redirect(url_for('user_management'))
+        return render_template('user_management.html', form=form)
+
+
+@app.route(rule='/user_management/password/<int:userid>', methods=['GET', 'POST'])
+@login_required
+def change_password(userid):
+    if current_user.permission_id != 1 or current_user.userid != userid:
+        flash('Sie haben keine Berechtigung für diese Seite', 'danger')
+        return redirect(url_for('index'))
+    else:
+        form = EditPasswordForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(userid=userid).first_or_404()
+            user.password_hash = generate_password_hash(form.password.data)
+            db.session.commit()
+            flash('Passwort erfolgreich geändert', 'success')
+            if current_user.permission_level == 1:
+                return redirect(url_for('user_management'))
+            else:
+                return redirect(url_for('index'))
+        return render_template('change_password.html', form=form)
+
+
+@app.route(rule='/user_management/permission/<int:userid>', methods=['GET', 'POST'])
+@login_required
+def change_permissions(userid):
+    if current_user.permission_id != 1:
+        flash('Sie haben keine Berechtigung für diese Seite', 'danger')
+        return redirect(url_for('index'))
+    else:
+        form = EditPermissionsForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(userid=userid).first_or_404()
+            user.permission_level = form.permission_level.data
+            db.session.commit()
+            flash('Berechtigungen erfolgreich geändert', 'success')
+            return redirect(url_for('user_management'))
+        return render_template('change_permissions.html', form=form)
+
+
+# TODO Remove db.create_all() after the first run
+db.create_all()
+app.run(debug=True)
--- forms.py
+++ forms.py
@@ -1,6 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, SelectField, TextAreaField, SelectMultipleField, widgets
+from wtforms.fields.datetime import DateField
 from wtforms.validators import DataRequired, EqualTo
+
+from models import Child, User
 
 
 class LoginForm(FlaskForm):
@@ -9,5 +12,64 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Anmelden')
 
 
-class ObservationForm(FlaskForm):
+class RegisterForm(FlaskForm):
+    username = StringField('Benutzername', validators=[DataRequired()])
+    password = PasswordField('Passwort', validators=[DataRequired(), EqualTo('confirm_password',
+                                                                             message='Passwörter stimmen nicht überein')])
+    confirm_password = PasswordField('Passwort bestätigen', validators=[DataRequired()])
+    given_name = StringField('Vorname')
+    surname = StringField('Nachname')
+    permission_level = SelectField('Berechtigungsstufe', choices=[(0, 'Benutzer'), (1, 'Administrator')], coerce=int)
+    submit = SubmitField('Registrieren')
+
+
+class EditPasswordForm(FlaskForm):
+    password = PasswordField('Neues Passwort', validators=[DataRequired()])
+    password2 = PasswordField('Passwort wiederholen', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Speichern')
+
+
+class EditPermissionsForm(FlaskForm):
+    permission_level = SelectField('Berechtigungen', choices=[
+        (0, 'Benutzer'),
+        (1, 'Administrator'),
+        (2, 'Deaktiviert')],
+                                   coerce=int)
+    submit = SubmitField('Speichern')
+
+
+class EditGroupForm(FlaskForm):
+    children = Child.query.sort_by(Child.given_name).all()
+    group_name = StringField('Gruppenname', validators=[DataRequired()])
+    children = SelectMultipleField('Kinder', choices=children.surname.given_name)
+
+
+class NewGroupForm(FlaskForm):
+    children = Child.query.sort_by(Child.given_name).all()
+    group_name = StringField('Gruppenname', validators=[DataRequired()])
+    children = SelectMultipleField('Kinder', choices=children.surname.given_name)
+
+
+class EditChildForm(FlaskForm):
+    betreuer = User.query.sort_by(User.surname).all()
+    given_name = StringField('Vorname', validators=[DataRequired()])
+    surname = StringField('Nachname', validators=[DataRequired()])
+    birth_date = DateField('Geburtsdatum', validators=[DataRequired()])
+    gender = SelectField('Geschlecht', choices=[('m', 'Männlich'), ('w', 'Weiblich'), ('d', 'Divers')],
+                         validators=[DataRequired()])
+    supervisor = SelectField('Betreuer', choices=[betreuer.surname.given_name], validators=[DataRequired()])
+
+
+class NewChildForm(FlaskForm):
+    betreuer = User.query.sort_by(User.surname).all()
+    given_name = StringField('Vorname', validators=[DataRequired()])
+    surname = StringField('Nachname', validators=[DataRequired()])
+    birth_date = DateField('Geburtsdatum', validators=[DataRequired()])
+
+
+class EditObservation(FlaskForm):
+    pass
+
+
+class NewObservation(FlaskForm):
     pass
